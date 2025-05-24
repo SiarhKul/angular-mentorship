ae4f651bb86cd2699a6a00919147aee47edd84c5
siarhkul <siarhkul@gmail.com>
2025-05-24 14:30:25 +0200

    chore: Add cors

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const FAKE_DB = [
+  {id: 1, username: "Jon", password: "12345"},
+  {id: 2, username: "Ann", password: "1qaz"},
+  {id: 3, username: "Bob", password: "2wsx"},
+]
+
+
 router.post('/', (req, res, next) => {
   console.log('-----------------', req.body)
-  res.json({jwt: '1234567890'});
+
+  const isUserExists = FAKE_DB.some((dbRecord) => {
+    return dbRecord.username === req.body.username
+  });
+  console.log('1111111111', isUserExists)
+  if (!isUserExists) {
+    throw new Error('User already exists');
+  }
+
+  res.json({jwt: crypto.randomUUID()});
 });
 
 module.exports = router;
