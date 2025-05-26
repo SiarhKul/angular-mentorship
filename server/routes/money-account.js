59edd242f411cf44419112778a2973bf1de4a524
siarhkul <siarhkul@gmail.com>
2025-05-26 22:42:03 +0200

    chore: Add id

--- server/routes/money-account.js
+++ server/routes/money-account.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { writeFile } = require("node:fs/promises");
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res) => {
   console.log("Received request for money accounts:", req.body);
 
-  res.json([{ id: 1 }]);
+  const account = req.body;
+  account.id = Date.now(); // Assign a unique ID based on the current timestamp
+
+  try {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    const data = JSON.stringify(req.body, null, 2);
+    await writeFile("db.json", data, { signal });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json(account);
 });
 
 module.exports = router;
