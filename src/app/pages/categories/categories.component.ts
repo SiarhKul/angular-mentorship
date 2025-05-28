acae5dd5df299efc1081bf7f6c6802a9013329d8
siarhkul <siarhkul@gmail.com>
2025-05-28 22:41:02 +0200

    chore: Add card layout

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -2,11 +2,15 @@ import {Component} from '@angular/core';
 import {
   AccountMoneyCreator
 } from "../../features/account-money-creator/account-money-creator";
+import {
+  AccountMoneyCard
+} from "../../features/account-money-creator/components/account-money-card";
 
 @Component({
   selector: 'app-categories',
   imports: [
     AccountMoneyCreator,
+    AccountMoneyCard
   ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
