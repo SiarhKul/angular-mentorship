6a90113854d02a28fe90ac22d97a42582969c1c5
siarhkul <siarhkul@gmail.com>
2025-05-28 21:13:53 +0200

    chore: Save accounts into db

--- server/routes/money-account.js
+++ server/routes/money-account.js
@@ -1,19 +1,34 @@
 const express = require("express");
+const { log } = require("node:console");
+const { read } = require("node:fs");
 const router = express.Router();
 const { writeFile } = require("node:fs/promises");
+const { readFile } = require("node:fs/promises");
 
 router.post("/", async (req, res) => {
   console.log("Received request for money accounts:", req.body);
 
   const account = req.body;
-  account.id = Date.now(); // Assign a unique ID based on the current timestamp
+  account.id = Date.now();
 
   try {
+    let accounts = [];
+
+    try {
+      const fileData = await readFile("db.json", "utf-8");
+      accounts = JSON.parse(fileData);
+      log("Existing accounts loaded:", accounts);
+    } catch (err) {
+      console.error("Error reading db.json:", err);
+      accounts = [];
+    }
+
     const controller = new AbortController();
     const { signal } = controller;
 
-    const data = JSON.stringify(req.body, null, 2);
-    await writeFile("db.json", data, { signal });
+    accounts.push(account);
+    log("New account added:", account);
+    await writeFile("db.json", JSON.stringify(accounts, null, 2), { signal });
   } catch (err) {
     console.error(err);
   }
