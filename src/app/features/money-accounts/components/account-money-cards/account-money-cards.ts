fa4d781751e9e3d0d07b9f977fcf2b7f74514fa6
Pavel Haurylik <phaurylik@exadel.com>
2025-06-06 23:44:52 +0200

    comments: suggestions

--- src/app/features/money-accounts/components/account-money-cards/account-money-cards.ts
+++ src/app/features/money-accounts/components/account-money-cards/account-money-cards.ts
@@ -1,12 +1,11 @@
-import {Component, Input} from '@angular/core'
-import {AccountMoneyService} from "../../services/api/account-money.service";
-import {AccountMoney} from "../../services/models/AccountMoney";
-
+import { Component, Input } from '@angular/core';
+import { AccountMoneyService } from '../../services/api/account-money.service';
+import { AccountMoney } from '../../services/models/AccountMoney';
 
 const CURRENCIES_DICTIONARY: Record<string, string> = {
-  'dollar': '$',
-  'euro': '€'
-}
+  dollar: '$',
+  euro: '€',
+};
 
 @Component({
   selector: 'account-money-cards',
@@ -16,23 +15,25 @@ const CURRENCIES_DICTIONARY: Record<string, string> = {
   providers: [AccountMoneyService],
   template: `
     @if (moneyAccounts; as accounts) {
-      <ul class="account-money-cards">
-        @for (account of accounts; track account.currency) {
-          <li class="account-money-card">
-            <div class="card-summary">
-              <span class="card-title">{{ account.typeCard }}</span>
-              <span
-                  class="currency">{{ CURRENCIES_DICTIONARY[account.currency] }}</span>
-            </div>
-            <span class="card-sum">{{ account.initSum }}</span>
-            {{ account.currency }}
-          </li>
-        }
-      </ul>
+    <ul class="account-money-cards">
+      @for (account of accounts; track account.currency + $index) {
+      <!-- без '+ $index' в консоле падали варнинги, указывающие что трек функция возвращает не уникальные значения, и ангуляр не мог следить корректно какие элементы перерисовывать, надо трекать по уникальному чему-то -->
+      <li class="account-money-card">
+        <div class="card-summary">
+          <span class="card-title">{{ account.typeCard }}</span>
+          <span class="currency">{{
+            CURRENCIES_DICTIONARY[account.currency]
+          }}</span>
+        </div>
+        <span class="card-sum">{{ account.initSum }}</span>
+        {{ account.currency }}
+      </li>
+      }
+    </ul>
     } @else {
-      <p>Loading...</p>
+    <p>Loading...</p>
     }
-  `
+  `,
 })
 export class AccountMoneyCards {
   @Input()
