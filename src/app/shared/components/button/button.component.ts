fefcf081fff0d22257d66218e956edc52437ca9f
siarhkul <siarhkul@gmail.com>
2025-06-08 13:26:11 +0200

    chore: Add stypes for button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,14 +1,16 @@
 import {Component, Input} from "@angular/core";
 import {PlusButtonComponent} from "../plus-button/plus-button.component";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-button',
   imports: [
-    PlusButtonComponent
+    PlusButtonComponent,
+    NgStyle
   ],
   styleUrl: './button.component.css',
   template: `
-    <div class="button">
+    <div class="button" [ngStyle]="customStyles">
       <app-plus-button [icon]="icon"/>
       <span>{{ buttonContent }}</span>
     </div>
@@ -19,4 +21,8 @@ export class ButtonComponent {
   buttonContent = 'Default Value'
   @Input()
   icon = 'add'
+
+  //todo: Ask mentor - real type for styles
+  @Input()
+  customStyles!: { [key: string]: string };
 }
