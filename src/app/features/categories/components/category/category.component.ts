cc2bddef728965dad02e5fb74be2b998725ffb14
siarhkul <siarhkul@gmail.com>
2025-06-14 09:38:49 +0200

    chore: Add mu stypes for buttons

--- src/app/features/categories/components/category/category.component.ts
+++ src/app/features/categories/components/category/category.component.ts
@@ -1,19 +1,29 @@
 import {Component, Input} from '@angular/core';
 import {NgClass} from "@angular/common";
 import {MatIcon} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css',
   imports: [
     NgClass,
     MatIcon,
-  ],
-  templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+    MatButtonModule
+  ]
 })
 export class CategoryComponent {
   @Input()
   name!: string;
   @Input()
   type!: string;
+
+  onEdit() {
+    console.log(1)
+  }
+
+  onDelete() {
+    console.log(2)
+  }
 }
