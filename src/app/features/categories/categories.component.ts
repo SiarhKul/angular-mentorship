95d15f490e72d55a3830c7a0c519aae3bef0d88e
Siarhei Kulazhanka <SiarhKul@gmail.com>
2025-06-16 07:33:09 +0200

    chore: Refactoring

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -1,7 +1,5 @@
 import {Component} from "@angular/core";
-import {
-  CategoriesCreator
-} from "./components/categories-creator/categories-creator";
+import {CategoriesCreator} from "./components/categories-creator/categories-creator";
 import {CategoriesListCtrl} from "./components/categories-list/categories-list";
 import {ICategory} from "./types/interfaces";
 import {CategoriesService} from "./services/categories.service";
@@ -9,40 +7,38 @@ import {Observable} from "rxjs";
 import {AsyncPipe} from "@angular/common";
 
 @Component({
-  selector: "app-categories",
-  styleUrl: "./categories.component.css",
-  standalone: true,
-  providers: [CategoriesService],
-  imports: [
-    CategoriesCreator,
-    CategoriesListCtrl,
-    AsyncPipe
-  ],
-  template: `
-    <section class="categories-container">
-      <app-categories-list [categories]="categories$| async"
-                           (categoryDeleted)="handleCategoryDeleted($event)"
-                           class="app-categories-list"/>
-      <app-categories-creator
-          (onSuccessSubmit)="handleOnSuccessSubmit($event)"/>
-    </section>`
+    selector: "app-categories",
+    styleUrl: "./categories.component.css",
+    standalone: true,
+    providers: [CategoriesService],
+    imports: [
+        CategoriesCreator,
+        CategoriesListCtrl,
+        AsyncPipe
+    ],
+    template: `
+        <section class="categories-container">
+            <app-categories-list [categories]="categories$| async"
+                                 (categoryDeleted)="handleCategoryDeleted($event)"
+                                 class="app-categories-list"/>
+            <app-categories-creator
+                    (onSuccessSubmit)="handleOnSuccessSubmit($event)"/>
+        </section>`
 })
 //todo: Mentor: Ask about naming convention 'handleOnSuccessSubmit'
 export class CategoriesComponent {
-  categories$: Observable<Required<ICategory>[]>
+    categories$: Observable<Required<ICategory>[]>
 
-  constructor(private cs: CategoriesService) {
-    this.categories$ = this.cs.getAllCategories()
+    constructor(private cs: CategoriesService) {
+        this.categories$ = this.cs.getAllCategories()
 
-  }
+    }
 
-  protected handleOnSuccessSubmit($event: Required<ICategory>) {
-    console.log("handleOnSuccessSubmit", $event);
-    this.categories$ = this.cs.getAllCategories()
-  }
+    protected handleOnSuccessSubmit($event: Required<ICategory>) {
+        this.categories$ = this.cs.getAllCategories()
+    }
 
-  protected handleCategoryDeleted(id: number) {
-    console.log("handleCategoryDeleted", id);
-    this.categories$ = this.cs.getAllCategories()
-  }
+    protected handleCategoryDeleted(id: number) {
+        this.categories$ = this.cs.getAllCategories()
+    }
 }
