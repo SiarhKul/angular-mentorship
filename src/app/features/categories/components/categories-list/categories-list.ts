36a1a39d0f3f205e0720360ba314a27a5fa7385f
Siarhei Kulazhanka <SiarhKul@gmail.com>
2025-06-16 07:48:30 +0200

    chore: Add prettier confit

--- src/app/features/categories/components/categories-list/categories-list.ts
+++ src/app/features/categories/components/categories-list/categories-list.ts
@@ -1,44 +1,46 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CategoriesService } from '../../services/categories.service';
-import { ICategory } from '../../types/interfaces';
-import { NgForOf } from '@angular/common';
-import { CATEGORIES } from '../../../../shared/constants/dictionaries';
-import { CategoryComponent } from '../category/category.component';
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { CategoriesService } from '../../services/categories.service'
+import { ICategory } from '../../types/interfaces'
+import { NgForOf } from '@angular/common'
+import { CATEGORIES } from '../../../../shared/constants/dictionaries'
+import { CategoryComponent } from '../category/category.component'
+
 
 @Component({
-  selector: 'app-categories-list',
-  styleUrl: './categories-list.css',
-  providers: [CategoriesService],
-  imports: [NgForOf, CategoryComponent],
-  template: `
-    <div class="categories">
-      <div *ngFor="let category of categories">
-        <app-category
-          [name]="category.name"
-          [type]="mappingIdToCategories[category.type]"
-          [id]="category.id"
-          (categoryDeleted)="onCategoryDeleted($event)"
-        />
-      </div>
-    </div>
-  `,
+    selector: 'app-categories-list',
+    styleUrl: './categories-list.css',
+    providers: [CategoriesService],
+    imports: [NgForOf, CategoryComponent],
+    template: `
+        <div class="categories">
+            <div *ngFor="let category of categories">
+                <app-category
+                        [name]="category.name"
+                        [type]="mappingIdToCategories[category.type]"
+                        [id]="category.id"
+                        (categoryDeleted)="onCategoryDeleted($event)"
+                />
+            </div>
+            
+        </div>
+    `,
 })
 export class CategoriesListCtrl {
-  mappingIdToCategories = _mappingIdToCategories;
-  @Input() categories!: Required<ICategory>[] | null;
-  @Output() categoryDeleted = new EventEmitter<number>();
+    mappingIdToCategories = _mappingIdToCategories;
+    @Input() categories!: Required<ICategory>[] | null;
+    @Output() categoryDeleted = new EventEmitter<number>();
 
-  onCategoryDeleted(id: number) {
-    this.categoryDeleted.emit(id);
-  }
+    onCategoryDeleted(id: number) {
+        this.categoryDeleted.emit(id);
+    }
 }
 
 const _mappingIdToCategories = CATEGORIES.reduce<Record<string, string>>(
-  (acc, currentValue) => {
-    return {
-      ...acc,
-      [currentValue.id]: currentValue.category,
-    };
-  },
-  {},
+    (acc, currentValue) => {
+        return {
+            ...acc,
+            [currentValue.id]: currentValue.category,
+        };
+    },
+    {},
 );
